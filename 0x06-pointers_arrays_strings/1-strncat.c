e2d99752d330c2aa37ca991db81f3ee062f99ccc
Haalatoon <rhilane.hala@gmail.com>
2023-11-15 08:24:37 -0800

    Task 1. strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings with some conditions
+ * @dest: first string
+ * @src: second string
+ * @n: the max of bytes to add from src
+ *
+ * Return: resulting string 
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = 0, i;
+
+	while (dest[len] != '\0')
+		len++;
+	for (i = 0; i < n; i++)
+		dest[len + i] = src[i];
+	dest[len + n] = '\0';
+	return (dest);
+}
