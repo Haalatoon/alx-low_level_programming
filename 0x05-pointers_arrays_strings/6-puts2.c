9637e6572bda07d00ae9eeb4188c0a64af659b1a
Haalatoon <rhilane.hala@gmail.com>
2023-11-15 06:44:28 -0800

    Task 6. Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,29 +1,20 @@
 #include "main.h"
 /**
- * puts2 - function should print only one character out of two
- * starting with the first one
- * @str: input
- * Return: print
+ * puts2 - prints every other character of a string, starting with the 1st char
+ * @str: parameter
+ *
  */
+
 void puts2(char *str)
 {
-	int longi = 0;
-	int t = 0;
-	char *y = str;
-	int o;
+	int n = 0; i;
 
-	while (*y != '\0')
+	while (str[n] != '\0')
+		n++;
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		y++;
-		longi++;
-	}
-	t = longi - 1;
-	for (o = 0 ; o <= t ; o++)
-	{
-		if (o % 2 == 0)
-	{
-		_putchar(str[o]);
-	}
+		_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }
