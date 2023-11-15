bbc8d23532b142e7919f3501ca02268ee61bedcf
Haalatoon <rhilane.hala@gmail.com>
2023-11-15 07:37:29 -0800

    Task 9. strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,10 +11,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int len = 0, i;
 
-	while (*src[len] != '\0')
+	while (src[len] != '\0')
 		len++;
 	for (i = 0; i < len; i++)
 		dest[i] = src[i];
-	dest[l] = '\0';
+	dest[len] = '\0';
 	return (dest);
 }
